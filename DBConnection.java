a6065802d7a4cef953316d90726475b8ed9cd1ac
Akanksha-mali6555 <akankshamali299@gmail.com>
2025-10-25 13:47:30 +0530

    Add files via upload

---
+++ DBConnection.java
@@ -0,0 +1,57 @@
+/*package db;
+import java.sql.*;
+
+public class DBConnection {
+    private static final String URL = "jdbc:mysql://localhost:3306/musicstreamingdb1";
+    private static final String USER = "root"; // change if needed
+    private static final String PASSWORD = "root123"; // your MySQL password
+
+    public static Connection getConnection() {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            return DriverManager.getConnection(URL, USER, PASSWORD);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+}
+*/
+package db;
+
+import model.User;
+import java.sql.*;
+
+public class DBConnection {
+
+    public static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(
+                "jdbc:mysql://localhost:3306/musicstreamingdb1",
+                "root",
+                "root123"
+        );
+    }
+
+    public static User getUserByUsername(String username) {
+        String query = "SELECT * FROM user WHERE User_ID = ?";
+        try (Connection conn = getConnection();
+             PreparedStatement ps = conn.prepareStatement(query)) {
+
+            ps.setString(1, username);
+            ResultSet rs = ps.executeQuery();
+            if (rs.next()) {
+                return new User(
+                        rs.getInt("id"),
+                        rs.getString("username"),
+                        rs.getString("email")
+                );
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return null; // user not found
+    }
+}
+
+
